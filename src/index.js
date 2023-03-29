6cbf11adaad7d0645448c979babfc9da64578502
Nazar Kobernyk <104902964+inkogi-to@users.noreply.github.com>
2023-03-29 18:19:07 +0100

    movie-route

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {App} from 'components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/inkogi-to'>
+    <BrowserRouter basename='/goit-react-hw-05-movies'>
       <App/>
     </BrowserRouter>
   </React.StrictMode>
